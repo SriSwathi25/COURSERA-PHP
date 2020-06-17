7df83ff24b1143676ba91519b393b1a36cd55ae8
SriSwathi25 <25nsriswathi@gmail.com>
2020-06-17 19:19:29 +0530

    done

--- week3-sessions and cookies/cookie_less_sessions.php
+++ week3-sessions and cookies/cookie_less_sessions.php
@@ -1,4 +1,23 @@
 <?php
 #to use sessions without cookies
 #send as post data or get data
+ini_set('session.use_cookies', 0);
+ini_set('session.use_only_cookies', 0);
+ini_set('session.use_trans_sid', 1);
+
 ?>
+<?php
+if(isset($_POST['PHPSESSID'])){
+    header("Location:cookie_less_sessions.php/PHPSESSID=".$_POST['PHPSESSID']);
+}
+else{
+    session_start();
+}
+
+?>
+<?php
+echo("<form method='POST'>");
+echo("<input type='submit' value='NEXT PAGE' />");
+echo("</form>");    
+?>
+<p>Session id : <?php echo(session_id()); ?></p>
