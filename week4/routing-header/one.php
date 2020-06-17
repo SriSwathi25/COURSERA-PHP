0e965be1750d55cd36801e1de88f08152c360e67
SriSwathi25 <25nsriswathi@gmail.com>
2020-06-17 22:02:18 +0530

    routing - practice

---
+++ week4/routing-header/one.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_POST['value'])){
+    if($_POST['value'] == 1){
+        header("Location:one.php");
+        return;
+    }
+    else if($_POST['value'] == 2){
+        header("Location:two.php");
+        return;
+    }
+    else if($_POST['value'] == 3){
+        header("Location:three.php");
+        return;
+    }
+}
+?>
+<html>
+<body>
+<h1>In page 1</h1>
+    <form method="POST">
+    <h1>Enter a number(1-3)</h1>
+    <input type="text" name="value" />
+    <br/>
+    <input type="submit" value="GO" />
+    </form>
+</body>
+</html>
